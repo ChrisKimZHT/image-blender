02a31de489541d024d9492e459470469a9f36377
ChrisKim <1515748548@qq.com>
2025-04-03 17:52:32 +0800

    [fix] fix array size overflow

--- src/process/resize2same.js
+++ src/process/resize2same.js
@@ -3,26 +3,26 @@ import { Image } from 'image-js';
 export const resize2same = (imageA, imageB) => {
   let imgA = imageA.clone(), imgB = imageB.clone();
   let width = imgA.width, height = imgA.height;
-  let resizeType = 0;
+  let resizeType = 0; // no resize
 
   if (imgA.width * imgB.height !== imgB.width * imgA.height) {
-    resizeType = 1;
+    resizeType = 1; // not same aspect ratio
     width = Math.max(imgA.width, imgB.width);
     imgA = imgA.resize({ width });
     imgB = imgB.resize({ width });
     if (imgB.height < imgA.height) {
       height = imgA.height;
-      let result = new Image(width, height, Array.from({ length: width * height * 4 }, () => 255));
+      let result = new Image(width, height, Uint8Array.from({ length: width * height * 4 }, () => 255));
       let offsetY = Math.round((height - imgB.height) / 2);
       imgB = result.insert(imgB.rgba8(), { x: 0, y: offsetY });
     } else {
       height = imgB.height;
-      let result = new Image(width, height, Array.from({ length: width * height * 4 }, () => 255));
+      let result = new Image(width, height, Uint8Array.from({ length: width * height * 4 }, () => 255));
       let offsetY = Math.round((height - imgA.height) / 2);
       imgA = result.insert(imgA.rgba8(), { x: 0, y: offsetY });
     }
   } else if (imgA.width !== imgB.width || imgA.height !== imgB.height) {
-    resizeType = 2;
+    resizeType = 2; // same aspect ratio but different size
     width = Math.max(imgA.width, imgB.width);
     height = Math.max(imgA.height, imgB.height);
     imgA = imgA.resize({ width, height });
